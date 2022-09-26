02cf5b0271e45de21d0efd4143b1b2d0ba92e780
georgeleonework <georgeleonework@gmail.com>
2022-09-26 12:17:09 -0400

    added search functionality for todos with filterTodos

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const generateTemplate = todo => {
 };
 
 addForm.addEventListener('submit', e => {
-
     e.preventDefault();
     const todo = addForm.add.value.trim(); //trim removes any white spaces
 
@@ -28,20 +27,28 @@ addForm.addEventListener('submit', e => {
 //delete todos
 
 list.addEventListener('click', e => {
-    if (e.target.classList.contains('delete')){
+    if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
 });
 
 const filterTodos = (term) => {
+
+    Array.from(list.children) //turns the list of children elements into an array
+        .filter((todo) => !todo.textContent.includes(term)) //the previous array is filtered by todo items. If the text content contains the input then we continue
+        .forEach(() => todo.classlist.add('filtered')); //for each of the ones that did not contained the text, were going to add a class to it
+
+    Array.from(list.children) 
+        .filter((todo) => todo.textContent.includes(term)) 
+        .forEach(() => todo.classlist.remove('filtered'));  //set the behavior for the ones that DO match
     
-}
+};
 
 //keyup event / every time a key is pressed the callback is ran and the term is set to the current value in the input
 search.addEventListener('keyup', () => {
     const term = search.value.trim();
     filterTodos(term);
-})
+});
 
 
 
